eaebcc858c20be87f3bde38c1a6b678e9d44e247
“luoqingfu” <746832476@qq.com>
2020-04-22 01:06:35 +0800

    新增project模型，group模型

--- use_api/admin.py
+++ use_api/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from use_api.models import Api
+from use_api.models import Api, Project, ApiGroup
 
 admin.site.register(Api)
-
-class Api(admin.ModelAdmin):
-    search_fields = ('name')
+admin.site.register(Project)
+admin.site.register(ApiGroup)
